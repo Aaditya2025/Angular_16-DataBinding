25756584d341429b2680b576765c3444ea644efe
Aditya Yadav <adityayadav135952@gmail.com>
2025-06-18 18:10:31 +0530

    minorChange

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,9 +1,9 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrls: ['./cockpit.component.css']
+  styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string , serverContent: string}>
